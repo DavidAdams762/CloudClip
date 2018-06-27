7d30a376dca1d3d0cea9c3d3c284fc7ae59744f1
David <davidadams_238@msn.com>
2018-06-27 18:23:47 +0200

    Now handling key creation and message saving on redis

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sys
 import socket
 import elasticache_auto_discovery
 import redis
+import json
 from errors import ClipError
 
 from pymemcache.client.hash import HashClient
@@ -11,8 +12,12 @@ from pymemcache.client.hash import HashClient
 #elasticache settings
 cache = redis.Redis(
     host='preprod-cloudclip2.xggwdh.0001.use2.cache.amazonaws.com',
-    port=6379
+    port=6379,
+    charset="utf-8",
+    decode_responses=True
     )
+#EXPIRE_TIME = 86400
+EXPIRE_TIME = 60
 
 def handler(event, context):
     """
@@ -21,11 +26,11 @@ def handler(event, context):
     """
     switcher = {
         "/newclip": addKey,
-        "/addclip": addClip
+        "/clip/{clip}": handleClip,
     }
     try:
         func = switcher.get(event["resource"])
-        func(event)
+        message = func(event)
     except ClipError as inst :
         message, code = inst.args
         return {
@@ -34,27 +39,79 @@ def handler(event, context):
             "isBase64Encoded": False
         }
 
+    #create an object that will hold the status code and body to use here
     response = {
         "statusCode": 200,
-        "body": "Successful",
+        "body": message,
         "isBase64Encoded": False
     }
     return response
 
 def addKey(event):
-    params = getParams(event)
-    if 'clip' not in params :
+    paramsObj = getParams(event)
+    query = paramsObj.queryParameters
+    if 'clip' not in query :
         raise ClipError("Clip parameter not found", 400)
-    key = params['clip']
+    if 'owner' not in query :
+        owner = "Annon"
+    key = query['clip']
     #Checking key availability
     if cache.get(key) != None :
         raise ClipError("Key already used", 400)
-    #cache.set(key, "Created", 86400)
-    cache.set(key, "Created", 60) #for tests
-    return True
+    cache.lpush(key, owner)
+    cache.expire(key, EXPIRE_TIME)
+    return "Key added succesfully"
 
-def addClip(event):
-    return event
+def addToClip(key, message):
+    cache.lpush(key, message)
+    cache.expire(key, EXPIRE_TIME)
+    amount = cache.llen(key)
 
-def getParams(event):
-    return event['queryStringParameters']
+    return {
+        'result' : 'ok',
+        'amount' : amount,
+    }
+
+def getParams(event, type = 'query'):
+    return ClipParameters(event['pathParameters'], event['queryStringParameters'])
+
+def handleClip(event):
+    paramsObj = getParams(event)
+    path = paramsObj.pathParameters
+    query = paramsObj.queryParameters
+    if 'clip' not in path :
+        raise ClipError("Clip parameter not found", 400)
+    key = path['clip']
+    if cache.exists(key) == 0:
+        raise ClipError("Key not found", 404)
+    method = event['httpMethod']
+    if method == 'GET':
+        page = path.get('page', 0)
+        response = getClipList(key, page)
+        print (response)
+        return json.dumps(response)
+    elif method == 'POST':
+        if 'message' not in query:
+            raise ClipError("No message", 400)
+        response = addToClip(key, query['message'])
+        return json.dumps(response)
+
+def getClipList(key, page):
+    list = cache.lrange(key, 20 * page, 20 * (page + 1))
+    amount = cache.llen(key)
+    cache.expire(key, EXPIRE_TIME)
+
+    result = {
+        'list' : list,
+        'amount' : amount,
+    }
+    return result
+
+
+class ClipParameters :
+    pathParameters = ''
+    queryParameters = ''
+
+    def __init__(self, path, query):
+        self.pathParameters = path
+        self.queryParameters = query
